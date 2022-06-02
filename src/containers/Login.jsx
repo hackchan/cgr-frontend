5b3a49aa1b5f6bbd9d90e35571f2403210ade7a1
fabio.rojas <fabiorojas7@gmail.com>
2022-06-02 17:42:54 -0500

    login form

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import '../styles/Login.css'
 export const Login = () => {
   const { register, handleSubmit, formState: { errors } } = useForm()
   const onSubmit = data => console.log(data)
   return (
-    <div className='login-box'>
-      <img src='https://i.ibb.co/fvZ0bL9/logo-cgr.png' alt='cgr' />
+    <div className='loginbox'>
+      <img className='avatar' src='https://i.ibb.co/fvZ0bL9/logo-cgr.png' alt='cgr' />
       <h1>Login in to Analizer</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* USERENAME */}
         <label htmlFor='username'>
           Useraname
           <input
-            type='text' placeholder='Full Name' name='username' id='username' {...register('username', {
+            type='text' placeholder='Username' name='username' id='username' {...register('username', {
               required: {
                 value: true,
                 message: 'el campo es requerido'
@@ -24,10 +25,10 @@ export const Login = () => {
         {errors.username && <span>{errors.username.message}</span>}
 
         {/* PASSWORD   */}
-        <label htmlFor='username'>
-          Useraname
+        <label htmlFor='password'>
+          Password
           <input
-            type='text' placeholder='Full Name' name='username' id='username' {...register('username', {
+            type='password' placeholder='Contraseña' name='password' id='password' {...register('password', {
               required: {
                 value: true,
                 message: 'el campo es requerido'
@@ -37,6 +38,9 @@ export const Login = () => {
           />
         </label>
         {errors.username && <span>{errors.username.message}</span>}
+        <input type='submit' value='Sign in' />
+        <a href='#'>lost your password</a>
+        <a href='#'>Register</a>
       </form>
     </div>
   )
